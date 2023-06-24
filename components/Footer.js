f68bdcd42efe18aa017400b3307b61391443077b
arturovilla <acvturi@gmail.com>
2023-06-23 19:21:47 -0500

    added some cool custon styling

--- components/Footer.js
+++ components/Footer.js
@@ -3,12 +3,12 @@ import {AiOutlineLinkedin, AiOutlineGithub, AiOutlineMail} from 'react-icons/ai'
 
 function Footer() {
   return (
-    <div className='bg-[#FF957F] flex-col items-center text-center py-16'>
+    <div className='bg-[#fc35ffdb] flex-col items-center text-center py-16'>
         <div className='flex justify-center '>
             <div className='flex justify-between w-1/2 md:w-1/5 '>
-                <AiOutlineLinkedin size={40} className='cursor-pointer' color='white'/>
-                <AiOutlineGithub size={40} className='cursor-pointer' color='white'/>
-                <AiOutlineMail size={40} className='cursor-pointer' color='white'/>
+                <AiOutlineLinkedin size={40} className='cursor-pointer' color='black'/>
+                <AiOutlineGithub size={40} className='cursor-pointer' color='black'/>
+                <AiOutlineMail size={40} className='cursor-pointer' color='black'/>
             </div>
         </div>
         
