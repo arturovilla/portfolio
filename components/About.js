3ced57089131a0ed6a7815c9ae733c6c5bd82591
arturovilla <acvturi@gmail.com>
2023-06-25 11:26:13 -0500

    refined the responsiveness of the page on different screens to not distract from the content

--- components/About.js
+++ components/About.js
@@ -1,41 +1,56 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import parse from 'html-react-parser';
+import React from "react";
+import { useState, useEffect } from "react";
+import parse from "html-react-parser";
 
 function About() {
-    const words = [
-        "<span className='font-extrabold italic' >Arturo Villalobos </span>", 
-        "<span className='font-extrabold italic' >a Software Engr 👨🏻‍💻</span>", 
-        "<span className='font-extrabold italic' >a Data Visualizer 📊</span>", 
-        "<span className='font-extrabold italic' >a Graphic Designer 👨🏼‍🎨</span>",
-        "<span className='font-extrabold italic' >a Tech Consultant 🧑🏻‍🏫</span>" ]
+	const words = [
+		"<span className='font-extrabold italic text-5xl md:text-7xl lg:text-8xl' >Arturo <br className='md:hidden' /> Villalobos</span>",
+		"<span className='font-extrabold italic text-5xl md:text-7xl lg:text-8xl' >a Software Engineer 👨🏻‍💻</span>",
+		"<span className='font-extrabold italic text-5xl md:text-7xl lg:text-8xl' >a Data Scientist 📊</span>",
+		"<span className='font-extrabold italic text-5xl md:text-7xl lg:text-8xl' >a Graphic Designer 👨🏼‍🎨</span>",
+		"<span className='font-extrabold italic text-5xl md:text-7xl lg:text-8xl' >a 3-D Modeler 🗜️</span>",
+	];
 
-    const [index, setIndex] = useState(0);
-    useEffect(() => {
-        const intervalDelayMilliseconds = words[index].length * 10;
-        const interval = setInterval(() => {
-          setIndex((prevIndex) => {
-            // reset index if current index is greater than array size
-            return prevIndex + 1 < words.length ? prevIndex + 1 : 0;
-          });
-        }, intervalDelayMilliseconds);
-    
-        return () => clearInterval(interval);
-    });
- 
-  return (
-    <div className=''>
-        <h1 className='text-left pl-5 text-[#E4DFDB] md:text-8xl md:pl-11 text-6xl w-full px-0 mx-0'>Hi, I&apos;m <span className='transition-all'>{parse(words[index])}</span></h1>
-        <p className='text-left pl-5 text-[#E4DFDB] text-md w-2/3 md:text-2xl md:pl-11 md:w-1/2 '>
-            <br/>
-            I am a graduate of the Texas A&M College of Engineering with a degree in computer science and a minor in statistics, with a concentration in data science. I come from a solid technical background with a strong interest in statistics and a passion for new technologies. My focus and interests vary from machine learning to web-development and graphic design.
-            <br/>
+	const [index, setIndex] = useState(0);
+	useEffect(() => {
+		const intervalDelayMilliseconds = words[index].length * 10;
+		const interval = setInterval(() => {
+			setIndex((prevIndex) => {
+				// reset index if current index is greater than array size
+				return prevIndex + 1 < words.length ? prevIndex + 1 : 0;
+			});
+		}, intervalDelayMilliseconds);
 
-            <br/>
-            As a freelance software engineer I like taking on projects in industries that one would not necessarily think about. Fashion, art, product design, and e-commerce are all things I have an interest in and am working towards getting to know more about. 
-        </p>
-    </div>
-  )
+		return () => clearInterval(interval);
+	});
+
+	return (
+		<div className="">
+			<div className=" text-[#E4DFDB] flex flex-col">
+				<h1 className=" text-left pl-5 pr-5 md:text-8xl md:pl-11 text-6xl w-full px-0 mx-0">
+					Hi, I&apos;m{" "}
+				</h1>
+				<span className=" transition-all text-left pl-5 pr-5 md:text-8xl md:pl-11 w-full px-0 mx-0">
+					{parse(words[index])}
+				</span>
+			</div>
+			<p className="text-left pl-5 text-[#E4DFDB] text-md w-2/3 md:text-2xl md:pl-11 md:w-1/2 ">
+				<br />
+				I am a graduate of the Texas A&M College of Engineering with a
+				degree in computer science and a minor in statistics, with a
+				concentration in data science. I come from a solid technical
+				background with a strong interest in statistics and a passion
+				for new technologies. My focus and interests vary from machine
+				learning to web-development and graphic design.
+				<br />
+				<br />
+				As a freelance software engineer I like taking on projects in
+				industries that one would not necessarily think about. Fashion,
+				art, product design, and e-commerce are all things I have an
+				interest in and am working towards getting to know more about.
+			</p>
+		</div>
+	);
 }
 
-export default About
+export default About;
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,128 +1,161 @@
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import staticLogo from "../public/0001.png";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import staticLogo from '../public/0001.png'
-
-import {AiOutlineMenu, AiOutlineClose, AiOutlineLinkedin, AiOutlineGithub, AiOutlineMail} from 'react-icons/ai'
-
-
-
+import {
+	AiOutlineMenu,
+	AiOutlineClose,
+	AiOutlineLinkedin,
+	AiOutlineGithub,
+	AiOutlineMail,
+} from "react-icons/ai";
 
 function Navbar() {
-  const email = 'artuvillam@gmail.com'
-  const [menuOpen, setMenuOpen] = useState(false)
-  const handleNav = () =>{
-    setMenuOpen(!menuOpen)
-  }
+	const email = "artuvillam@gmail.com";
+	const [menuOpen, setMenuOpen] = useState(false);
+	const handleNav = () => {
+		setMenuOpen(!menuOpen);
+	};
 
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
-  const [visible, setVisible] = useState(true)
+	const [prevScrollPos, setPrevScrollPos] = useState(0);
+	const [visible, setVisible] = useState(true);
 
-  const handleScroll = () => {
-    const currentScrollPos = window.scrollY
+	const handleScroll = () => {
+		const currentScrollPos = window.scrollY;
 
-    if(currentScrollPos > prevScrollPos){
-        setVisible(false)
-    }else{
-        setVisible(true)
-    }
+		if (currentScrollPos > prevScrollPos) {
+			setVisible(false);
+		} else {
+			setVisible(true);
+		}
 
-    setPrevScrollPos(currentScrollPos)
-  }
-  useEffect( () => {
-    window.addEventListener('scroll', handleScroll);
+		setPrevScrollPos(currentScrollPos);
+	};
+	useEffect(() => {
+		window.addEventListener("scroll", handleScroll);
 
-    return () => window.removeEventListener('scroll', handleScroll)
-  })
+		return () => window.removeEventListener("scroll", handleScroll);
+	});
 
-  /////////////////////////////////////////////
-  return (
+	/////////////////////////////////////////////
+	return (
+		<nav className={`sticky w-full h-32  ${visible ? "top-0" : ""} `}>
+			<div className="flex justify-between items-center h-full w-full px-4 2xl:px-16 py-24 md:py-32">
+				<div className="">
+					<Link href="/">
+						<Image
+							src={staticLogo}
+							alt="spinning gif saying a.dev"
+							className="cursor-pointer"
+							width="auto"
+							height="300"
+						/>
+					</Link>
+				</div>
+				<div className="hidden sm:flex">
+					<ul className="flex text-white">
+						<Link href="/work">
+							<li className="ml-10 hover:border-b text-lg py-1">
+								Work
+							</li>
+						</Link>
 
-    <nav className={`sticky w-full h-32  ${visible ? 'top-0' : ''} `}>
-        <div className='flex justify-between items-center h-full w-full px-4 2xl:px-16 py-24 md:py-32'>
-          <div className=''>
-            <Link href="/">
-              <Image
-              src={staticLogo}
-              alt="spinning gif saying a.dev"
-              className="cursor-pointer"
-              width="auto"
-              height="300"/>
-            </Link>
-          </div>
-          <div className='hidden sm:flex'>
-            <ul className='flex text-white'>
-            
-            <Link href="/work">
-                <li className='ml-10 hover:border-b text-lg py-1'>Work</li>
-            </Link>
-            
-            <Link href="/resume">
-                <li className='ml-10 hover:border-b text-lg py-1'>Resume</li>
-            </Link>
-            <Link href="/contact">
-                <li className='ml-10 text-lg py-1 px-5 text-white rounded-md border border-white hover:bg-white hover:text-black hover:shadow-2xl hover:shadow-stone-300 transition duration-150 ease-in-out'>Contact</li>
-            </Link>
-            </ul>
-          </div>
-          {/* mobile menu icon*/}
-          <div className='sm:hidden flex justify-end items-center h-full w-full'>
-            
-            <div onClick={handleNav} className='sm:hidden pl-20 cursor-pointer '> 
-              <AiOutlineMenu size={25} color='white'/>
-            </div>
-          </div>
-        </div>
-        {/* mobile menu */}
-        <div className={
-          menuOpen
-          ? "fixed left-0 top-0 w-[100%] sm:hidden h-screen bg-gray-900/70 backdrop-blur-md p-10 ease-in duration-500"
-          : "fixed left-[-100%] top-0 p-10 ease-in duration-500"
-        }>
-          {/* inside mobile menu */}
-          <div className='flex w-full items-center justify-end '>
-            <div onClick={handleNav} className='cursor-pointer'>
-              <AiOutlineClose size={20} color='white'/>
-            </div>
-          </div>
-          <div className='flex-col items-center text-center text-5xl py-4 text-white'>
-            <ul>
-              <Link href="#">
-                <li
-                onClick={() => setMenuOpen(false)} 
-                className='hover:border-b pt-10'>Work</li>
-              </Link>
-              
-            
-              <Link href="/resume">
-                <li
-                onClick={() => setMenuOpen(false)} 
-                className=' hover:border-b pt-10'>Resume</li>
-              </Link>
-              <Link href="#">
-                <li
-                onClick={() => setMenuOpen(false)} 
-                className='hover:border-b pt-10'>Contact</li>
-              </Link>
-            </ul>
-          </div>
-          {/* socials, legal, and other links inside the mobile menu */}
-          <div className='flex flex-row justify-around pt-52 items-center'>
-            <a href='https://www.linkedin.com/in/rtvro/' target="_blank">
-              <AiOutlineLinkedin size={40} className='cursor-pointer' color='white'/>
-            </a>
-            <a href='https://github.com/arturovilla' target="_blank">
-              <AiOutlineGithub size={40} className='cursor-pointer' color='white'/>
-            </a>
-            <a href={`mailto:${email}`}>
-              <AiOutlineMail size={40} className='cursor-pointer' color='white'/>
-            </a>
-          </div>
-        </div>
-    </nav>
+						<Link href="/resume">
+							<li className="ml-10 hover:border-b text-lg py-1">
+								Resume
+							</li>
+						</Link>
+						<Link href="/contact">
+							<li className="ml-10 text-lg py-1 px-5 text-white rounded-md border border-white hover:bg-white hover:text-black hover:shadow-2xl hover:shadow-stone-300 transition duration-150 ease-in-out">
+								Contact
+							</li>
+						</Link>
+					</ul>
+				</div>
+				{/* mobile menu icon*/}
+				<div className="sm:hidden flex justify-end items-center h-full w-full">
+					<div
+						onClick={handleNav}
+						className="sm:hidden pl-20 cursor-pointer "
+					>
+						<AiOutlineMenu size={25} color="white" />
+					</div>
+				</div>
+			</div>
+			{/* mobile menu */}
+			<div
+				className={
+					menuOpen
+						? "fixed left-0 top-0 w-[100%] sm:hidden h-screen bg-gray-900/70 backdrop-blur-md p-10 ease-in duration-500"
+						: "fixed left-[-100%] top-0 p-10 ease-in duration-500"
+				}
+			>
+				{/* inside mobile menu */}
+				<div className="flex w-full items-center justify-end ">
+					<div onClick={handleNav} className="cursor-pointer">
+						<AiOutlineClose size={20} color="white" />
+					</div>
+				</div>
+				<div className="flex-col items-center text-center text-5xl py-4 text-white">
+					<ul>
+						<Link href="#">
+							<li
+								onClick={() => setMenuOpen(false)}
+								className="hover:border-b pt-10"
+							>
+								Work
+							</li>
+						</Link>
 
-  )
+						<Link href="/resume">
+							<li
+								onClick={() => setMenuOpen(false)}
+								className=" hover:border-b pt-10"
+							>
+								Resume
+							</li>
+						</Link>
+						<Link href="/contact">
+							<li
+								onClick={() => setMenuOpen(false)}
+								className="hover:border-b pt-10"
+							>
+								Contact
+							</li>
+						</Link>
+					</ul>
+				</div>
+				{/* socials, legal, and other links inside the mobile menu */}
+				<div className="flex flex-row justify-around pt-52 items-center">
+					<a
+						href="https://www.linkedin.com/in/rtvro/"
+						target="_blank"
+					>
+						<AiOutlineLinkedin
+							size={40}
+							className="cursor-pointer"
+							color="white"
+						/>
+					</a>
+					<a href="https://github.com/arturovilla" target="_blank">
+						<AiOutlineGithub
+							size={40}
+							className="cursor-pointer"
+							color="white"
+						/>
+					</a>
+					<a href={`mailto:${email}`}>
+						<AiOutlineMail
+							size={40}
+							className="cursor-pointer"
+							color="white"
+						/>
+					</a>
+				</div>
+			</div>
+		</nav>
+	);
 }
 
-export default Navbar
+export default Navbar;
