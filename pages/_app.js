524e993611990458f51c42b72e699b89e1becdc8
arturovilla <acvturi@gmail.com>
2023-08-17 18:25:16 -0500

    added analytics thing from vercel

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,23 @@
-import Navbar from '@/components/Navbar'
-import Footer from '@/components/Footer'
-import '@/styles/globals.css'
-import localFont from 'next/font/local'
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
+import "@/styles/globals.css";
+import localFont from "next/font/local";
+import { Analytics } from "@vercel/analytics/react";
 
 const Druk = localFont({
-  src: '..//public/fonts/Druk_Wide_Bold.ttf',
-  variable: '--font-Druk-bold',
-})
-
+	src: "..//public/fonts/Druk_Wide_Bold.ttf",
+	variable: "--font-Druk-bold",
+});
 
 export default function App({ Component, pageProps }) {
-  return (
-    <main className={`${Druk.variable}`}>
-      <Navbar/>
-      <Component {...pageProps} />
-      <div className='mt-40'>
-        <Footer/>
-      </div>
-    </main>
-  )
+	return (
+		<main className={`${Druk.variable}`}>
+			<Navbar />
+			<Component {...pageProps} />
+			<Analytics />
+			<div className="mt-40">
+				<Footer />
+			</div>
+		</main>
+	);
 }
