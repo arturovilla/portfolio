e6b1a56c280a8b96d17a7df11c3980deb56c0073
Arturo Villalobos <artuvillam@gmail.com>
2025-09-12 17:21:52 -0500

    updated resume and file

--- pages/resume.js
+++ pages/resume.js
@@ -3,7 +3,7 @@ import useDownloader from "react-use-downloader";
 
 function Resume() {
 	const { download } = useDownloader();
-	const fileUrl = "/arturoresume.pdf";
+	const fileUrl = "/ArturoVillalobosResume.pdf";
 	const filename = "arturo_villalobos_resume.pdf";
 
 	return (
@@ -26,7 +26,7 @@ function Resume() {
 					<p className="text-xs pt-3 pb-2">
 						<a href="mailto:artuvillam@gmail.com" className="text-gruvpink hover:text-gruvtan transition ease-in-out">artuvillam@gmail.com</a> |
 						<a href="https://www.linkedin.com/in/rtvro/" target="_blank" className="text-gruvpink hover:text-gruvtan transition ease-in-out"> https://www.linkedin.com/in/rtvro/</a> |
-						<a href="https://github.com/arturovilla" target="_blank" className="text-gruvpink hover:text-gruvtan transition ease-in-out"> https://github.com/arturovilla</a> | 
+						<a href="https://github.com/arturovilla" target="_blank" className="text-gruvpink hover:text-gruvtan transition ease-in-out"> https://github.com/arturovilla</a> |
 						<a href="https://www.rtvro.dev" target="_blank" className="text-gruvpink hover:text-gruvtan transition ease-in-out"> rtvro.dev</a>
 					</p>
 					<hr className="" />
@@ -38,16 +38,13 @@ function Resume() {
 					<div className="text-left pb-2">
 						<ul className="list-disc pl-6">
 							<li>
-								Java, Python 3+, Javascript, HTML/CSS, Rstats, C++.
+								Java, Python 3+, C#, Javascript, HTML/CSS, Php, Rstats, C++.
 							</li>
 							<li>
 								MSSQL/SQL, MongoDB, Redis, NoSQL, AWS/Azure, Docker, Ansible.
 							</li>
 							<li>
-								Platforms and Tools: Git Version Control, VScode, Xcode, Unreal Engine 4, Blender
-							</li>
-							<li>
-								AWS Certified Cloud Practitioner Certification | Issued June 2020- Expires June 2023
+								Platforms and Tools: Teams, Slack, Jira, Azure DevOps, VScode, Xcode, JetBrains Products
 							</li>
 							<li>
 								Languages: English-Native, Spanish-Native,
@@ -61,45 +58,34 @@ function Resume() {
 					<h2 className="font-semibold pt-3">WORK EXPERIENCE</h2>
 					<div className=" pb-2">
 						<div className="flex justify-between font-medium text-md">
-							<h3><span className="text-gruvpink">Software Engineer</span>, Standard Data Inc.</h3>
+							<h3><span className="text-gruvpink">Senior Software Engineer</span>, <a href="https://standarddata.ai" className="text-gruvgreen hover:text-gruvtan transition ease-in-out">Standard Data, Inc.</a> Houston, TX.</h3>
 							<h3> Aug 2023 - Present</h3>
 						</div>
 						<div className="text-left pb-2 text-sm w-10/12">
 							<ul class="list-disc pl-6">
 								<li>
-									Engineered and implemented micro-services for ML-enhanced maintenance calculations in military and industrial applications, 
-									seamlessly integrating diverse environments and technologies to optimize performance and scalability.
+								  Develop and deploy micro-services using Java Spring Boot, TypeScript, MongoDB, and Microsoft SQL Server to deliver machine learning–driven maintenance calculations, ensuring seamless integration across military and industrial systems with diverse performance and scalability requirements.
 								</li>
 								<li>
-									Lead the CI/CD initiatives for comprehensive project deployment, orchestrating front-end and back-end integration using Azure Pipelines, 
-									Ansible, and Docker to ensure seamless and efficient delivery.
+								  Lead CI/CD implementation using Azure Pipelines, Ansible, and Docker to automate and integrate front-end and back-end deployments, streamlining project delivery.
 								</li>
 								<li>
-									Partnered with project stakeholders to define requirements, conduct demonstrations, 
-									and deploy live versions of projects to Air Force bases nationwide, ensuring alignment with strategic objectives and operational readiness.
+								  Partner with project stakeholders to define requirements, conduct demonstrations, and deploy live versions of projects to various customers nationwide, ensuring alignment with strategic objectives and operational readiness.
+								</li>
+								<li>
+									Develop and maintain a critical deployment and data synchronization tool using C#, Ansible, and Docker, essential for live demonstrations and ensuring up-to-date application data, driving greater customer adoption.
+								</li>
+								<li>
+								  Adapt quickly to organizational needs: originally hired as a Python developer, learn Java to support a major architectural shift; collaborate as one of three backend engineers to transition from micro-services (Python, Redis, MongoDB, Java) to a monolithic design, successfully delivering a beta test within six months.
+								</li>
+								<li>
+								  Develop PHP micro-services for an automotive sensor manufacturer and re-architect the backend to transition from MongoDB to Elastic-search, centralizing data storage and optimizing time-series query performance.
 								</li>
 							</ul>
 						</div>
-						<div className="flex justify-between font-medium text-md">
-							<h3><span className="text-gruvpink">Web Developer</span>, Blyss Dating (Remote)</h3>
-							<h3> Mar 2023 - July 2023</h3>
-						</div>
-						<div className="text-left pb-2 text-sm w-10/12">
-							<ul class="list-disc pl-6">
-								<li>
-									Develop and deploy a maintainable, and user-friendly business website leveraging modern technologies such as React/Next.js, Tailwind CSS, and AWS. 
-									This includes designing intuitive user interfaces, ensuring responsive performance, and implementing best practices.
-								</li>
-								<li>
-									Integrate a headless content management system (CMS) using GraphQL to empower the Blyss newsroom and blog capabilities. 
-									This integration enables team members to seamlessly add and manage content, ensuring a flexible and efficient content delivery system that enhances the overall workflow.
-								</li>
-							</ul>
-						</div>
-
 						<div className="flex justify-between font-medium text-md">
 							<h3>
-								<span className="text-gruvpink">Software Developer</span>, Classic Canvas Company, San
+								<span className="text-gruvpink">Software Developer</span>, <a href="https://cccliftbags.com/?srsltid=AfmBOorqJGVz8F6bR0FzA4TqpL1TYKh3EKgYAaQy7UDTorDUoeuys77z" className="text-gruvgreen hover:text-gruvtan transition ease-in-out">Classic Canvas Company</a>, San
 								Antonio, TX.
 							</h3>
 							<h3>Feb 2023 - June 2023</h3>
@@ -107,39 +93,34 @@ function Resume() {
 						<div className="text-left pb-2 text-sm w-10/12">
 							<ul class="list-disc pl-6">
 								<li>
-									Develop internal automation tools and a web interface using React, Python, and AWS to boost social media presence and activity. 
-									These tools will streamline marketing efforts, allowing for increased engagement and reach without the need for additional marketing resources.
+								  Developed internal automation tools and a web interface using React, Python, and AWS to boost social media presence and activity. These tools will streamline marketing efforts, allowing for increased engagement and reach without the need for additional marketing resources.
 								</li>
 								<li>
-									Create and maintain comprehensive documentation detailing the tools used, specific system design decisions, and other relevant information. 
-									This documentation will serve as a valuable resource for current and future team members, ensuring transparency, consistency, and ease of knowledge transfer within the organization.
+								  Created and delivered comprehensive documentation detailing the tools used, specific system design decisions, and other relevant information. This documentation will serve as a valuable resource for current and future team members, ensuring transparency, consistency, and ease of knowledge transfer within the organization.
 								</li>
 							</ul>
 						</div>
 
 						<div className="flex justify-between font-medium text-md">
 							<h3>
-								<span className="text-gruvpink">Cloud Solutions Architect Internship</span>, University
-								Lands, Houston, TX
+								<span className="text-gruvpink">Cloud Solutions Architect Internship</span>, <a href="https://universitylands.utsystem.edu" className="text-gruvgreen hover:text-gruvtan transition ease-in-out">University Lands</a>, Houston, TX
 							</h3>
 							<h3>May 2020 - Aug 2020</h3>
 						</div>
 						<div className="text-left pb-2 text-sm w-10/12">
 							<ul class="list-disc pl-6">
 								<li>
-									Develop and deploy server-less Lambda functions in Python on the AWS cloud to automate the ETL (Extract, Transform, Load) processes for data from Drilling and Completion reports in the Permian Basin. 
-									This solution enhances data processing efficiency, scalability, and accuracy, streamlining the integration of critical oil well data into the system.
+								  Developed server-less Lambda functions in Python on the AWS cloud to automate the ETL (Extract, Transform, Load) processes for data from Drilling and Completion reports in the Permian Basin. This solution enhances data processing efficiency, scalability, and accuracy, streamlining the integration of critical oil well data into the system.
 								</li>
 								<li>
-									Design and implement architecture utilized by chemical and mechanical engineers to construct type curves and facilitate cost analysis of current wells. 
-									This architecture supports advanced data analysis and decision-making processes, enhancing the efficiency and accuracy of engineering evaluations and financial assessments. 
+								  Designed and developed a cloud architecture utilized by chemical and mechanical engineers to construct type curves and facilitate cost analysis of current wells. This architecture supports advanced data analysis and decision-making processes, enhancing the efficiency and accuracy of engineering evaluations and financial assessments.
 								</li>
 							</ul>
 						</div>
 
 						<div className="flex justify-between font-medium text-md">
 							<h3>
-								<span className="text-gruvpink">Peer Teacher</span>, Texas A&M College of Engineering,
+								<span className="text-gruvpink">Peer Teacher</span>, <a href="https://engineering.tamu.edu/index.html" className="text-gruvgreen hover:text-gruvtan transition ease-in-out">Texas A&M College of Engineering</a>,
 								College Station, TX.
 							</h3>
 							<h3>Aug 2020 - Dec 2021</h3>
@@ -147,11 +128,11 @@ function Resume() {
 						<div className="text-left pb-2 text-sm w-10/12">
 							<ul class="list-disc pl-6">
 								<li>
-									Tutor undergraduate and graduate students enrolled in Computer Science and Engineering courses, providing guidance and support both remotely and in-person. 
+									Tutored undergraduate and graduate students enrolled in Computer Science and Engineering courses, providing guidance and support both remotely and in-person.
 									This includes helping students understand complex concepts, improve their problem-solving skills, and succeed academically through personalized instruction and mentorship.
 								</li>
 								<li>
-									Taught courses including Data Structures and Algorithms, Introduction to Computing, Computer Organization, Discrete Mathematics, and Linear Algebra under the supervision of a professor. 
+									Taught courses including Data Structures and Algorithms, Introduction to Computing, Computer Organization, Discrete Mathematics, and Linear Algebra under the supervision of a professor.
 									This involved preparing lectures, grading assignments, and offering individualized support to enhance students&apos; understanding and performance in these subjects.
 								</li>
 							</ul>
@@ -181,43 +162,16 @@ function Resume() {
 					<h2 className="font-semibold pt-3">PROJECTS</h2>
 					<div className="pb-2">
 						<div className="flex justify-between font-medium text-md">
-							<h3>Juego - Lead Developer - React Web App</h3>
-							<h3>Nov 2021- Dec 2021</h3>
+							<h3><span className="text-gruvpink">Creative Developer</span>, <a href="https://www.artclubhtx.com" className="text-gruvgreen hover:text-gruvtan transition ease-in-out">Art Club</a>, Houston, TX</h3>
+							<h3>Dec 2024 - Present</h3>
 						</div>
 						<div className="text-left pb-2 text-sm w-10/12">
 							<ul class="list-disc pl-6">
 								<li>
-									Led a team of developers to launch a social
-									network platform that connected users with
-									similar interests with respect to video
-									games and other media using a PostgreSQL
-									database, React framework, and Node.JS
-									backend.
+								  Design and build custom real-time 3D art installations for music artists, leveraging TouchDesigner, Blender, and advanced mathematical techniques to deliver dynamic visuals for weekly live performances. These interactive visual systems enhance audience engagement and adapt seamlessly to diverse performance environments.
 								</li>
 								<li>
-									Using an agile development methodology I set
-									targets, tracked progress, and delivered our
-									working product.
-								</li>
-							</ul>
-						</div>
-
-						<div className="flex justify-between font-medium text-md">
-							<h3>
-								Olympic Index - Co-Developer - Design Project{" "}
-							</h3>
-							<h3>Sep 2021 - Dec 2021</h3>
-						</div>
-						<div className="text-left pb-2 text-sm w-10/12">
-							<ul class="list-disc pl-6">
-								<li>
-									Developed a responsive, design focused, data
-									visualization project with React.js, D3.js,
-									and Python to explore historical olympic
-									data with a variety of charts, each
-									highlighting different aspects of the data
-									to reveal underlying and interesting
-									patterns.
+								  Maintain and repair high-performance servers powering real-time visualizations and gallery-wide installations, ensuring reliability and minimal downtime.
 								</li>
 							</ul>
 						</div>
