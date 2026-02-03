9166f518e2f50d2b48110f06e613b7ce6697fbe7
Arturo Villalobos <artuvillam@gmail.com>
2026-02-03 01:39:57 -0600

    adding patreon and gumroad links

--- components/Footer.js
+++ components/Footer.js
@@ -2,47 +2,43 @@ import React from "react";
 import {
 	AiOutlineLinkedin,
 	AiOutlineGithub,
-	AiOutlineMail,
+  AiOutlineMail
 } from "react-icons/ai";
-
+import { SiPatreon, SiGumroad } from "react-icons/si";
 const email = "artuvillam@gmail.com";
-function Footer() {
-	return (
-		<div className="bg-pink-900  flex-col items-center text-center py-16 sticky top-[100vh] mt-52">
-			<div className="flex justify-center ">
-				<div className="flex justify-between w-1/2 md:w-1/5 ">
-					<a
-						href="https://www.linkedin.com/in/rtvro/"
-						target="_blank"
-					>
-						<AiOutlineLinkedin
-							size={40}
-							className="cursor-pointer"
-							color="black"
-						/>
-					</a>
-					<a href="https://github.com/arturovilla" target="_blank">
-						<AiOutlineGithub
-							size={40}
-							className="cursor-pointer"
-							color="black"
-						/>
-					</a>
-					<a href={`mailto:${email}`}>
-						<AiOutlineMail
-							size={40}
-							className="cursor-pointer"
-							color="black"
-						/>
-					</a>
-				</div>
-			</div>
 
-			<div className=" mt-4">
-				<p>Created by Arturo Villalobos. © 2023</p>
-			</div>
-		</div>
-	);
+function Footer() {
+  return (
+    <div className="bg-pink-900 flex-col items-center text-center py-16 sticky top-[100vh] mt-52">
+      <div className="flex justify-center">
+        <div className="flex justify-between w-1/2 md:w-1/5">
+          <a href="https://www.linkedin.com/in/rtvro/" target="_blank" rel="noreferrer">
+            <AiOutlineLinkedin size={40} className="cursor-pointer" color="black" />
+          </a>
+
+          <a href="https://github.com/arturovilla" target="_blank" rel="noreferrer">
+            <AiOutlineGithub size={40} className="cursor-pointer" color="black" />
+          </a>
+
+          <a href={`mailto:${email}`}>
+            <AiOutlineMail size={40} className="cursor-pointer" color="black" />
+          </a>
+
+          <a href="https://patreon.com/Programare" target="_blank" rel="noreferrer">
+            <SiPatreon size={34} className="cursor-pointer" color="black" />
+          </a>
+
+          <a href="https://rturo.gumroad.com/l/strangeattractors" target="_blank" rel="noreferrer">
+            <SiGumroad size={34} className="cursor-pointer" color="black" />
+          </a>
+        </div>
+      </div>
+
+      <div className="mt-4">
+        <p>Created by Arturo Villalobos. © 2023</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
